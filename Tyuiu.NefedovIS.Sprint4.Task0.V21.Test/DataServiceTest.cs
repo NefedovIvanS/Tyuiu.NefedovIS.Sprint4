9c2f5ebb966d3d3bbb664078426e3a01b197f99e
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-06 22:35:36 +0500

    Add project files.

---
+++ Tyuiu.NefedovIS.Sprint4.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint4.Task0.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint4.Task0.V21/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.NefedovIS.Sprint4.Task0.V21.Lib;
+namespace Tyuiu.NefedovIS.Sprint4.Task0.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int[] a = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************\n");
+
+            Console.WriteLine("a = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 }\n");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetSumEvenArrEl(a));
+        }
+    }
+}
