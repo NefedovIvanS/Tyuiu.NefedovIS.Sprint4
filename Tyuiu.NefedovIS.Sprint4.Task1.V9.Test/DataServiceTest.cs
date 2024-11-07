60e35ad2dcb5a92d31a2c1bbcb3796e7d9272f9b
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-07 13:00:34 +0500

    Task1

---
+++ Tyuiu.NefedovIS.Sprint4.Task1.V9.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint4.Task1.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint4.Task1.V9/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.NefedovIS.Sprint4.Task1.V9.Lib;
+namespace Tyuiu.NefedovIS.Sprint4.Task1.V9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************\n");
+
+            Console.Write("Введите кол-во элементов: ");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] a = new int[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.Write("Введите значение " + i + " элемента массива: ");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a));
+        }
+    }
+}
