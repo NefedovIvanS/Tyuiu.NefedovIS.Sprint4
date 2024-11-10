4e26f84c4b40aee947a35f509672c8abbb25d894
NefedovIvanS <Tyuiu.NefedovIS@outlook.com>
2024-11-10 13:44:42 +0500

    Task6

---
+++ Tyuiu.NefedovIS.Sprint4.Task6.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.NefedovIS.Sprint4.Task6.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.NefedovIS.Sprint4.Task6.V21/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.NefedovIS.Sprint4.Task6.V21.Lib;
+namespace Tyuiu.NefedovIS.Sprint4.Task6.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string[] a = ["Facebook", "Twitter", "Instagram", "Snapchat", "LinkedIn", "Pinterest", "Reddit"];
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(a));
+        }
+    }
+}
